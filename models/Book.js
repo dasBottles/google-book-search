c3e183ba2e7c3b973d179d92206d06bf0e686602
David <arattanasack@gmail.com>
2020-03-27 17:52:14 -0700

    initial commit

---
+++ models/Book.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+   title: String,
+   author: String,
+   desc: String,
+   img: String,
+   link: String 
+});
+
+const Book = mongoose.model('Book', BookSchema);
+module.exports = Book;
